0175ef20cfff9d2851f6459c402476a3c5356efb
beth-em <beth.murillo318@gmail.com>
2025-04-08 22:55:50 -0600

    updated token authorizations in auth.ts, added authentication to index.ts and created function for JWT token to return if user and password are correct and exist

--- Develop/server/src/middleware/auth.ts
+++ Develop/server/src/middleware/auth.ts
@@ -7,4 +7,29 @@ interface JwtPayload {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: verify the token exists and add the user data to the request object
+  // Get token from Authorization header
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; 
+
+  if (!token) {
+    return res.status(401).json({message: 'Access token is missing!'});
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined in .env');
+    }
+
+    const decoded = jwt.verify(token, secret) as JwtPayload;
+
+    // Attach users info to the request
+    req.user = decoded;
+    next();
+    return;   // Added next() and return; to avoid typescript warning
+    // Function to move to the next middleware
+    next();
+  } catch (err) {
+    return res.status(403).json({message: 'Your token is invalid or expired token!'});
+  }
 };
--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -3,8 +3,40 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response): Promise<void> => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { username } });
+  
+    if (!user) {
+     res.status(401).json({ message: 'Invalid username or password' });
+     return;
+    }
+
+    // Function to compare hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+     res.status(401).json({ message: 'Invalid username or password' });
+     return;
+    }
+
+    // Create and sign JWT
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '1hr' }
+    );
+
+    res.json({ token });
+    return;
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error during login :('});
+    return;
+  }
 };
 
 const router = Router();
