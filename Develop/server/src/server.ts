94e21d0adc3144e1e80cb4a63d699b459fefbde5
beth-em <beth.murillo318@gmail.com>
2025-04-12 13:26:18 -0600

    fixed issue in server.ts that prevented render from reading index.html correctly

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -1,25 +1,30 @@
-const forceDatabaseRefresh = false;
-
-import dotenv from 'dotenv';
-dotenv.config();
-
+import path from 'path';
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
-import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const forceDatabaseRefresh = false;
+
+// Middleware to parse JSON
+app.use(express.json());
 
 // Serves static files in the entire client's dist folder
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// API routes
+app.use(routes);
+
+// Catch all handler to serve React's index.html for any other requests
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
-app.use(express.json());
-app.use(routes);
-
+// Synch models and start server
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
