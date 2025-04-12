73356b67ecba03077178256a3e174f01f0fa5ce4
beth-em <beth.murillo318@gmail.com>
2025-04-11 23:24:21 -0600

    server.ts errors when rendering correct

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -6,12 +6,16 @@ dotenv.config();
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 
 app.use(express.json());
 app.use(routes);
